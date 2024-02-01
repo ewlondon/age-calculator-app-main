09183474179d611c878dd043e9ef614be6942e1c
Eric London <llithikk@Gmail.com>
2024-02-01 04:00:23 -0500

    final commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const age = {
 	day: '',
 	month: '',
 	year: '',
-};
+};//object for storing input date
 
 function handleChange(e) {
 	if (e.name === 'Day')
@@ -28,23 +28,23 @@ function handleChange(e) {
 		const [yourYears, yourMonths, yourDays] = calculateAge(
 			`${age.year}-${age.month}-${age.day}`
 		);
-		animateCountUp(dayResult,yourDays, 80);
-		animateCountUp(monthResult,yourMonths, 80);
-		animateCountUp(yearResult,yourYears, 80);
-
-	} else {
+		animateCountUp(dayResult, yourDays, 80);
+		animateCountUp(monthResult, yourMonths, 80);
+		animateCountUp(yearResult, yourYears, 80);
+	} // if date is valid we set the result
+	else {
 		dayResult.innerText = '--';
 		monthResult.innerText = '--';
 		yearResult.innerText = '--';
-	}
-}
+	} // date input is invalid so we set the result to --
+}//input handling
 
 function animateCountUp(inputField, targetNumber, duration) {
 	const startNumber = 0;
 	const intervalTime = Math.max(
 		10,
 		duration / Math.abs(targetNumber - startNumber)
-	); 
+	);
 	const increment = targetNumber > startNumber ? 1 : -1;
 
 	let currentNumber = startNumber;
@@ -58,14 +58,14 @@ function animateCountUp(inputField, targetNumber, duration) {
 			currentNumber += increment;
 		}
 	}, intervalTime);
-}
+}//animates a count up from 0 to target of input
 
 function validDate(age) {
 	if (age.day.length <= 1) return false;
 	if (age.month.length < 1) return false;
 	if (age.year.length <= 3) return false;
 	return true;
-}
+}//alidation to check that date input is of correct lengths
 
 function calculateAge(birthdate) {
 	const birthDateObj = new Date(birthdate);
@@ -96,4 +96,4 @@ function calculateAge(birthdate) {
 	}
 
 	return [ageInYears, ageInMonths, ageInDays];
-}
+}//calculate the age in years, months and days
